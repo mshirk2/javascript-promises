75c350ae3678e5e178089605c8563a579a8af308
mshirk2 <michelle.l.shirk@gmail.com>
2022-09-23 16:33:24 -0400

    question 3

--- part-2/app.js
+++ part-2/app.js
@@ -1,6 +1,6 @@
-// 1.
-
 url = 'http://deckofcardsapi.com/api/deck';
+
+// 1.
 
 let newDeck1 = axios.get(`${url}/new/shuffle/?deck_count=1`);
 let deckID1 = newDeck1.deck_id;
@@ -9,7 +9,6 @@ let newCard = axios.get(`${url}/${deckID1}/draw/?count=1`)
 	.then(console.log(newCard.value," of ", newCard.suit));
 
 // 2.
-url = "http://deckofcardsapi.com/api/deck";
 
 let newDeck2 = axios.get(`${url}/new/shuffle/?deck_count=1`);
 let deckID2 = newDeck2.deck_id;
@@ -17,7 +16,14 @@ let deckID2 = newDeck2.deck_id;
 let firstCard = axios.get(`${url}/${deckID2}/draw/?count=1`);
 let secondCard = axios.get(`${url}/${deckID2}/draw/?count=1`)
 	.then(console.log(firstCard.value," of ", firstCard.suit))
-.then(console.log(secondCard.value," of ", secondCard.suit));
+	.then(console.log(secondCard.value," of ", secondCard.suit));
 
 // 3.
+let $button = $("#draw");
+let getDeck = axios.get(`${url}/new/shuffle/?deck_count=1`)
+let deckID = getDeck.deck_id;
 
+$button.on('click', () => {
+	let card = axios.get(`${url}/${deckID}/draw/?count=1`)
+		.then(console.log(card.value," of ", card.suit));
+});
