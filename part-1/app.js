9c657cbd07c12cb92240dc3017c3cfd4c349c512
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-09-24 12:26:22 -0400

    finish exercise

--- part-1/app.js
+++ part-1/app.js
@@ -1,25 +1,22 @@
-let url = 'http://numbersapi.com/'
+let url = 'http://numbersapi.com'
 let num = 9
 
-// 1.
-axios.get(`${url}${num}?json`)
-    .then(data => {console.log(data)});
+// // 1.
+// axios.get(`${url}/${num}?json`)
+//     .then(res => {console.log(res.data)});
 
-// 2.
-let nums = [2,9]
-axios.get(`${url}${nums}?json`)
-    .then(data => {console.log(data)});
+// // 2.
+// let nums = [2,9]
+// axios.get(`${url}/${nums}?json`)
+//     .then(res => {console.log(res.data)});
 
 // 3.
 Promise.all(
     Array.from({ length: 4 }, () => {
-        return axios.get(`${url}${num}?json`);
+        return axios.get(`${url}/${num}?json`);
     })
-)
-    .then(data => {console.log(data)})
-    .then(res => { 
-        res.forEach(data => 
-            document.getElementById('container')
-            .append(`${data.text}`)
-        );
-    });
+).then(res => { 
+    console.log(res)
+    res.forEach(fact =>
+        $('#container').append(`<p>${fact.data.text}</p>`)
+    )});
